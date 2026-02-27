12a2d2d508ec5e9cad8ba234899d4d83893fd9e1
sami.qaladzey <yarakayhawrem@gmail.com>
2026-02-27 03:28:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,23 @@
-self.addEventListener('push', function(event) {
-    const data = event.data.json();
-    const options = {
-        body: data.body,
-        icon: 'https://raw.githubusercontent.com/ramiqaladzay0/ramadan-challenge/450380601a15e12a80be482ee5149dd6dc52a0de/D5208D5D-BECD-4A90-9B8F-B7FC0679E5FA.jpeg',
-        badge: 'https://raw.githubusercontent.com/ramiqaladzay0/ramadan-challenge/450380601a15e12a80be482ee5149dd6dc52a0de/D5208D5D-BECD-4A90-9B8F-B7FC0679E5FA.jpeg'
-    };
-    event.waitUntil(self.registration.showNotification(data.title, options));
-});
-
 self.addEventListener('install', (event) => {
     self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+// گوێگرتن لە نامەکان بۆ ناردنی نۆتفیکەیشن
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SCHEDULE_NOTIFICATION') {
+        const { title, body, delay } = event.data;
+        
+        setTimeout(() => {
+            self.registration.showNotification(title, {
+                body: body,
+                icon: 'https://raw.githubusercontent.com/ramiqaladzay0/ramadan-challenge/450380601a15e12a80be482ee5149dd6dc52a0de/D5208D5D-BECD-4A90-9B8F-B7FC0679E5FA.jpeg',
+                vibrate: [200, 100, 200],
+                badge: 'https://raw.githubusercontent.com/ramiqaladzay0/ramadan-challenge/450380601a15e12a80be482ee5149dd6dc52a0de/D5208D5D-BECD-4A90-9B8F-B7FC0679E5FA.jpeg'
+            });
+        }, delay);
+    }
 });
