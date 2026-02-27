cd8331f62ebe67a5203bd85b29dec192f7d953ae
sami.qaladzey <yarakayhawrem@gmail.com>
2026-02-27 15:37:26 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,13 @@
-const CACHE_NAME = 'athan-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('message', event => {
-  if (event.data && event.data.type === 'SCHEDULE_NOTIFICATION') {
-    const { title, body, delay } = event.data;
-    
-    setTimeout(() => {
-      self.registration.showNotification(title, {
-        body: body,
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
         icon: 'https://raw.githubusercontent.com/ramiqaladzay0/ramadan-challenge/450380601a15e12a80be482ee5149dd6dc52a0de/D5208D5D-BECD-4A90-9B8F-B7FC0679E5FA.jpeg',
-        vibrate: [200, 100, 200, 100, 200, 100, 200],
-        requireInteraction: true
-      });
-      
-      self.clients.matchAll().then(clients => {
-        clients.forEach(client => {
-          client.postMessage({ type: 'PLAY_ATHAN' });
-        });
-      });
-    }, delay);
-  }
+        badge: 'https://raw.githubusercontent.com/ramiqaladzay0/ramadan-challenge/450380601a15e12a80be482ee5149dd6dc52a0de/D5208D5D-BECD-4A90-9B8F-B7FC0679E5FA.jpeg'
+    };
+    event.waitUntil(self.registration.showNotification(data.title, options));
+});
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
 });
